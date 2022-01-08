f2fcf2d919d7f3b2a73c5150b737111d505ad1d8
AnnaxT <82735488+AnnaxT@users.noreply.github.com>
2022-01-08 12:26:38 +0000

    adjust font size and height

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -8,9 +8,10 @@ export default function Display({ topValues, bottomValue }) {
     let fontSize, bn = bottomValue.length;
 
     if (bn <=8) fontSize = '3rem';
-    if (bn > 8 ) fontSize = '2.5rem';
-    if (bn > 13) fontSize = '2rem'; 
-    if (bn >14 ) fontSize = '1.25rem';
+    if (bn > 8 ) fontSize = '2.75rem';
+    if (bn > 10) fontSize = '2rem'; 
+    if (bn >13 ) fontSize = '1.5rem';
+    if (bn > 17)fontSize = '1.25rem'
     
     return(
 <div className="Display">
